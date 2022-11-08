36e16809f0043b9794b842ddef9e679d933999dd
matsamb <matondo.lusamba@gmail.com>
2022-11-08 12:19:05 +0100

    initial commit

---
+++ src/test/java/com/medi/parent/ParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medi.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
